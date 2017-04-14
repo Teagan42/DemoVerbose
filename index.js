654607ff7cf0820b35b4f86126f9c0d04479fa36
Teagan Glenn <teagan@massroots.com>
2017-04-13 20:22:28 -0600

    Add api description

--- index.js
+++ index.js
@@ -8,28 +8,29 @@ var jsonParser = bodyParser.urlencoded();
 app.use(jsonParser);
 /**
  * @api {post} /api/:id Create User information
+ * @apiDescription Creates a new user on the database
  * @apiName PostUser
  * @apiGroup User
  *
- * @apiParam {Number} id Users unique ID.
- *
+ * @apiParam {String} name Users name.
  */
 app.post('/api/', function(req, res, next) {
   var body = req.body;
   console.log(body);
-  if (!body.id || !/\d+/.test(body.id)) {
-    return next(new HttpError.BadRequest("Invalid id"));
-  }
   if (!body.name) {
     return next(new HttpError.BadRequest("Invalid name"));
   }
 
-  return res.send();
+  return res.send({
+    id: 123
+  });
 });
 
 
 /**
  * @api {get} /api/:id Get User information
+ * @apiDescription Retrieves a user based on 
+ * the user's unique identifier
  * @apiName GetUser
  * @apiGroup User
  *
@@ -52,6 +53,8 @@ app.get('/api/:id', function(req, res, next) {
 
 /**
  * @api {put} /api/:id Update User information
+ * @apiDescription Updates the user record with the given
+ * unique identifier
  * @apiName PutUser
  * @apiGroup User
  *
